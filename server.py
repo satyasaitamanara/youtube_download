8bf3602d66389f41a4037a853df99f6a86f7e1d6
satyasaitamanara <tamanarasatyasai@gmail.com>
2025-05-23 10:42:15 +0530

    Update server.py

--- server.py
+++ server.py
@@ -46,4 +46,5 @@ async def download_video(request: Request):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=int(os.environ.get("PORT", 8000)))
+    port = int(os.environ.get("PORT", 8000))  # Fallback for local dev
+    uvicorn.run(app, host="0.0.0.0", port=port)
